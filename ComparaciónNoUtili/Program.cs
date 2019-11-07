e5a6dd0f5b45ef8daa96cff4a1b96941f902291a
AvMexico2019 <56038497+AvMexico2019@users.noreply.github.com>
2019-11-07 16:36:40 -0600

    agrupacion por folios de prueba

--- ComparaciónNoUtili/Program.cs
+++ ComparaciónNoUtili/Program.cs
@@ -10,6 +10,49 @@ namespace ComparaciónNoUtili
     {
         static void Main(string[] args)
         {
+            ArrendamientoInmuebleEntities ctx = new ArrendamientoInmuebleEntities();
+            var tablaTest = ctx.NoUtiliAuditoria2019T;
+            var tablaProd = ctx.NoUtiliAuditoria2019TProduccion;
+            foreach (var test in tablaTest)
+            {
+                Console.WriteLine("/" + test.FolioContratoArrto + "/");
+                var result = (from prod in tablaProd
+                              where test.FolioContratoArrto == prod.FolioContratoArrto
+                              select new
+                              {
+                                  FolioContrato = test.FolioContratoArrto,
+                                  IDInmueble = test.IdInmuebleArrendamiento,
+                                  IDInmuebleProd = prod.IdInmuebleArrendamiento,
+                                  Inst = test.Fk_IdInstitucion,
+                                  InstProd = prod.Fk_IdInstitucion,
+                                  FechaFin = test.FechaFinOcupacion,
+                                  FechaFinProd = prod.FechaFinOcupacion
+                              });
+                foreach (var r in result)
+                {
+                    DateTime fecha = r.FechaFin != null ? (DateTime)r.FechaFin :
+                    new DateTime(1990, 1, 1);
+                    DateTime fechaProd = r.FechaFinProd != null ? (DateTime)r.FechaFinProd :
+                    new DateTime(1990, 1, 1);
+                    Console.WriteLine("-" + r.IDInmueble + "-" + r.IDInmuebleProd + "-" +
+                        r.Inst + "-" + r.InstProd + "-" +
+                        fecha + "-" + fechaProd); 
+                }
+            }
+            
+            /*
+            foreach(NoUtiliAuditoria2019T renglonTest in tablaTest)
+            {
+                DateTime fecha = renglonTest.FechaFinOcupacion != null ? (DateTime) renglonTest.FechaFinOcupacion :
+                    new DateTime(1990,1,1);
+                Console.WriteLine("Test " + renglonTest.FolioContratoArrto + " " +
+                                            renglonTest.IdInmuebleArrendamiento + " " +
+                                            renglonTest.Fk_IdInstitucion + " " +
+                                            fecha.ToString("yyyy MMMM dd"));
+                
+            }
+            */
+            Console.ReadLine();
         }
     }
 }
